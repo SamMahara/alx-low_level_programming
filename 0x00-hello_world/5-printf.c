3b482a46c47bc662154216b6c952614dc8c7e9f1
SamMahara <mzamomahara@gmail.com>
2023-02-20 19:05:45 -0800

    Fix 5-printf.c for Betty Style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,7 +10,8 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,");
+printf("\n")
 return (0);
 }
 
