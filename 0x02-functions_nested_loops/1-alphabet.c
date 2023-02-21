111ccf4299959ea87a0ee3930b59d7bc748ff30f
SamMahara <mzamomahara@gmail.com>
2023-02-21 05:02:24 -0800

    Fix 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-/**
- * print_alphabet - prints the lowercase alphabet followed by a new line
- *
- * Return: void
- */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
 void print_alphabet(void)
 {
-char c = 'a';
-
-while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
-_putchar('\n');
+    int i;
+    for (i = 97; i <= 122; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
 }
 
