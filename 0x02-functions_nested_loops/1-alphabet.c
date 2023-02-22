5c5842bb0b3332982ef58734e964c8abcb8c6523
SamMahara <mzamomahara@gmail.com>
2023-02-21 18:28:50 -0800

    Fix 0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
-#include "main.h"
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-    print_alphabet();
-    return (0);
-}
-
-void print_alphabet(void)
-{
-    int i;
-    for (i = 97; i <= 122; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+	return (write(1, &c, 1));
 }
 
