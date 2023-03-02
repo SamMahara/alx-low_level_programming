ac85ec6bd258f900687263f59c18d0fd73b08084
SamMahara <mzamomahara@gmail.com>
2023-03-02 03:40:06 -0800

    Add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: Pointer to first string
+ * @s2: Pointer to second string
+ *
+ * Return: Difference in ASCII value of first non-matching character
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+	if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+}
+
+return (s1[i] - s2[i]);
+}
+
