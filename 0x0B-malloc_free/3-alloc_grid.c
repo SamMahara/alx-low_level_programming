556531a9122a25d6f994044d2c4a75b3ed50a9cd
SamMahara <mzamomahara@gmail.com>
2023-03-14 19:10:14 -0700

    Fix 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,17 +22,17 @@ if (grid == NULL)
 
 for (i = 0; i < height; i++)
 {
-	grid[i] = malloc(sizeof(int) * width);
-        if (grid[i] == NULL)
+grid[i] = malloc(sizeof(int) * width);
+	if (grid[i] == NULL)
         {
             /* free previously allocated memory */
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return (NULL);
+		for (j = 0; j < i; j++)
+			free(grid[j]);
+		free(grid);
+		return (NULL);
         }
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
+	for (j = 0; j < width; j++)
+		grid[i][j] = 0;
 }
 
 return (grid);
